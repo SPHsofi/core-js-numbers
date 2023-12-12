ae71a0ac9cf8ab7d258a035b577f47f13599edf1
SPHsofi <Sofia_pash@mail.ru>
2023-12-12 20:54:41 +0300

    feat: 12 (toNumber) task solved

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -239,8 +239,10 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const res = Number(value) ? value : def;
+  return res;
+  // throw new Error('Not implemented');
 }
 
 /**
