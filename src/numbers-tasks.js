a5477b1b8a1087a2327c9cceff4f8c26994e367c
SPHsofi <Sofia_pash@mail.ru>
2023-12-13 11:01:30 +0300

    feat: 35, 17 (getRandomInteger, isPowerOfTwo) task solved

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -324,8 +324,9 @@ function getSumOfDigits(/* num */) {
  *   16  => true
  *   15  => false
  */
-function isPowerOfTwo(/* num */) {
-  throw new Error('Not implemented');
+function isPowerOfTwo(num) {
+  return Number.isInteger(Math.log2(num));
+  // throw new Error('Not implemented');
 }
 
 /**
@@ -618,8 +619,9 @@ function getMaxNumber(firstNumber, secondNumber) {
  * -5, 0 => -5 | -4 | -3 | -2 | -1 | 0
  * -1, 1 => -1 | 0 | 1
  */
-function getRandomInteger(/* min, max */) {
-  throw new Error('Not implemented');
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+  // throw new Error('Not implemented');
 }
 
 /**
